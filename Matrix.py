411eb8c222dbe05e575cf1b502d55400f2587ef9
paren <parengalvan@gmail.com>
2010-02-05 15:43:25 -0800

    Soporte para nt

--- Matrix.py
+++ Matrix.py
@@ -1,12 +1,18 @@
 import Matrix
 import os
 from time import sleep
+from Who import Who
+
+im = Who()
 
 class Matrix:
 
     #Propiedades (Herramientas de trabajo)
     scanr = 'nmap'
-    tracer = 'traceroute'
+    if im.sistema(im.sistema_operativo, True) == "nt":
+        tracer = "tracert"
+    else:
+        tracer = 'traceroute'
     ider = 'whois'
     ping = 'ping'
     diger = 'dig'
@@ -14,14 +20,24 @@ class Matrix:
     # Metodo para hacer pings
     def pingeame(self, host):
         pinger = Matrix.ping
-        verificacion_pinger = os.popen(pinger + ' ' +'-c 1 ' + ' ' + host, 'r')
+        if im.sistema(im.sistema_operativo, True) == "nt":
+            verificacion_pinger = os.popen(pinger + ' ' +'-n 1 ' + ' ' + host, 'r')
+        else:
+            verificacion_pinger = os.popen(pinger + ' ' +'-c 1 ' + ' ' + host, 'r')
         i = 0
         for line in verificacion_pinger:
             i += 1
-            if i == 5:
-                print line
-                print ' '
-                sleep(1)
+            if im.sistema(im.sistema_operativo, True) == "nt":
+                if i == 7:
+                    print line
+                    print ' '
+                    sleep(1)
+            else:
+                if i == 5:
+                    print line
+                    print ' '
+                    sleep(1)
+
 
     #Metodo para hacer una traza a un host
     def traceme(self, host):
@@ -45,19 +61,24 @@ class Matrix:
                 
     #Metodo para revisar informacion Whois
     def whom(self, host):
-        idme = Matrix.ider
-        print ''
-        d = os.popen(idme + ' ' + host + ' | ' + ' tail -n25 ', 'r')
-
-        print ' '
-        print d.read()
-        print ' '
-        sleep(1)
+        if im.sistema(im.sistema_operativo, True) == "nt":
+            print "Aun no soportado para este sistema: ", im.sistema(im.sistema_operativo, False) 
+        else:
+            idme = Matrix.ider
+            print ''
+            d = os.popen(idme + ' ' + host + ' | ' + ' tail -n25 ', 'r')
+            print ' '
+            print d.read()
+            print ' '
+            sleep(1)
 
     #Metodo para revisar informacion de records DNS
     def dig(self, host):
-        diger = Matrix.diger
-        record = str(raw_input('Que record deseas: '))
-        print ''
-        g = os.popen(diger + ' ' + record.upper() + ' ' + host)
-        print g.read()
+        if im.sistema(im.sistema_operativo, True) == "nt":
+            print "Aun no soportado para este sistema: ", im.sistema(im.sistema_operativo, False) 
+        else:
+            diger = Matrix.diger
+            record = str(raw_input('Que record deseas: '))
+            print ''
+            g = os.popen(diger + ' ' + record.upper() + ' ' + host)
+            print g.read()
